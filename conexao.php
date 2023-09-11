e25b5098e4484252232c61ff21e5eddfc6052a00
Maria Eduarda Cândido Alves Guerra <99684296+mariaeduardaguerra@users.noreply.github.com>
2023-09-11 09:28:45 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php 
+    define ('HOST', 'localhost');
+    define ('USER', 'root');
+    define('PASS', '');
+    define('BASE', 'sistema2');
+    $conn = new MySQLi(HOST, USER, PASS, BASE);
+
+
+    // Cria de definições para a conexão com o banco de dados
+    ?>
+
+    
